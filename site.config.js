9eee0e215ad0ba05ed47e477761e1f3efea4d8e4
JB Won <jim8697@snu.ac.kr>
2023-11-06 19:00:03 +0900

    site file update

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "hypeodive",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "developer",
+    bio: "Just a bunch of geeks who are passionate about engineering and love to share our knowledge with the world.",
+    email: "jim8697@gmail.com",
+    linkedin: "jongbum-won-020586228",
+    github: "hypeodive",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `enginism.com`,
+      href: "https://github.com/hypeodive/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "enginism.com",
+    description: "welcome to enginism.com!",
   },
 
   // CONFIG configration (required)
@@ -47,7 +47,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
